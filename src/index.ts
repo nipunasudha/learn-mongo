a7a3cfaab5dc189fe2a26d0647d5b0a6281dd893
Nipuna Sudharaka <nipuna.sudha@gmail.com>
2021-07-19 09:05:55 +0530

    Implement basic mongo crud

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Abc')
+import {AnyError} from "mongodb";
+
+const {MongoClient} = require('mongodb');
+
+console.log('Starting Library Application');
+
+const uri = "mongodb+srv://testusername:testpassword@cluster0.0qe2y.mongodb.net/libraryDb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(async (err: AnyError | undefined) => {
+    const collection = client.db("libraryDb").collection("books");
+    // perform actions on the collection object
+    const bookCount = await collection.countDocuments();
+    console.log(bookCount);
+    await collection.insertOne({
+        'name': 'My Book 3',
+        'author': 'My Author 3',
+    })
+    await client.close();
+});
