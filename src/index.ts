12e8c601c5a4ba6ebb380b4fb97a7d581cd8b026
Nipuna Sudharaka <nipuna.sudha@gmail.com>
2021-07-19 09:10:51 +0530

    Refactor mongo code

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import {AnyError} from "mongodb";
-
 const {MongoClient} = require('mongodb');
 
 console.log('Starting Library Application');
 
 const uri = "mongodb+srv://testusername:testpassword@cluster0.0qe2y.mongodb.net/libraryDb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-client.connect(async (err: AnyError | undefined) => {
+
+async function main(): Promise<void> {
+    await client.connect();
     const collection = client.db("libraryDb").collection("books");
-    // perform actions on the collection object
     const bookCount = await collection.countDocuments();
     console.log(bookCount);
-    await collection.insertOne({
-        'name': 'My Book 3',
-        'author': 'My Author 3',
-    })
+    /*    await collection.insertOne({
+            'name': 'My Book 3',
+            'author': 'My Author 3',
+        })*/
     await client.close();
-});
+}
+
+main().catch(console.error);
